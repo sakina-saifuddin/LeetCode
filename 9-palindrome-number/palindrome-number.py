00dc41ab8eff4a77dd0b66ae45e9c5ed1336218a
ML Engineer | Data Analytics & Predictive Modeling <sakinasaifee006@gmail.com>
2026-02-24 09:17:55 -0500

    Time: 267 ms (5.24%) | Memory: 12.4 MB (56.22%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        print("hi", str(x))
+        newx=str(x)
+        
+        backli=""
+        
+      
+
+        print(newx)
+        backli=newx[::-1]
+        
+        if backli == newx:
+            return True
+        else:
+            return False
+        
+
+        print(backli)
+
+        
+
+        #     c=-
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
