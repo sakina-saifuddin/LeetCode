cacb7b9bef3585951c0c562996ac97b078d9cc95
ML Engineer | Data Analytics & Predictive Modeling <sakinasaifee006@gmail.com>
2026-04-01 18:17:10 -0500

    Time: 11 ms (56.33%) | Memory: 19.3 MB (71.92%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,17 +1,20 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
 
+        #k=2
+
         maxlen=0
         l=0
         dicti={}
 
         for r, char in enumerate(s):
-            dicti[char]=dicti.get(char, 0)+1
+            dicti[char]=dicti.get(char, 0)+1  #adding char with tis count in the dict
 
         
         for key, values in dicti.items():
             if values < k:
-                parts=s.split(key)
+                parts=s.split(key) #splitting teh string on bad character. e.g. if key=b, value=1 which means that value=1 < k=2
+                #then we split the entire string on that character
                 return max(self.longestSubstring(part, k) for part in parts)
 
           
