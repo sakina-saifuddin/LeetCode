42ccf448ee1dd01b99fb95f120a9df1f76762030
ML Engineer | Data Analytics & Predictive Modeling <sakinasaifee006@gmail.com>
2026-04-06 12:24:40 -0500

    Time: 44 ms (9.82%) | Memory: 27.7 MB (22.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,18 +4,77 @@ class Solution:
         n=len(nums)
         left=[1]*n
         right=[1]*n
+
         product=[1]*n
-        #building left array, everything except index 0
-        for i in range(1,n):
-            left[i]=left[i-1] * nums[i-1]
 
-        for i in range(n-2,-1,-1):
-            right[i]=right[i+1] * nums[i+1]
+        #calculate everythig on the left of the ith value
+        for i in range(1,len(nums)):
+            left[i]=left[i-1]*nums[i-1]
 
-        for i in range(n):
+        #calculate everythig on the left of the ith value
+        for i in range(len(nums)-2, -1, -1):
+            right[i]=right[i+1]*nums[i+1]
+
+        print(left)
+        print(right)
+
+        
+        for i in range(len(nums)):
             product[i]=left[i]*right[i]
 
         return product
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n=len(nums)
+        # left=[1]*n
+        # right=[1]*n
+        # product=[1]*n
+        # #building left array, everything except index 0
+        # for i in range(1,n):
+        #     left[i]=left[i-1] * nums[i-1]
+
+        # for i in range(n-2,-1,-1):
+        #     right[i]=right[i+1] * nums[i+1]
+
+        # print(right)
+        # print(left)
+        # for i in range(n):
+        #     product[i]=left[i]*right[i]
+
+        # return product
 
         
         
