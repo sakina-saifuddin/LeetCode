50e89a7cf7d98ef7908afbf3e08ba6d28ba803d6
ML Engineer | Data Analytics & Predictive Modeling <sakinasaifee006@gmail.com>
2026-04-11 20:51:07 -0400

    Time: 248 ms (48.63%) | Memory: 21.5 MB (77.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,79 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        totalrows=len(grid)
-        totalcols=len(grid[0])
+        
+        rows=len(grid)
+        cols=len(grid[0])
         island=0
+        direction=[[1,0], [-1,0], [0,1], [0,-1]]
 
-        q=deque()
-        visit=set() #to avoid duplicates, and handle our visited positions
-        direction=[[0,1], [0,-1], [1,0], [-1,0]]
+        def dfs(r,c):
 
-        def bfs(self, r, c): #so we will see the adjacent values of the current positon 
-             q.append((r, c)) #q=[(0,0)]
-             visit.add((r,c))
+            if r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0":
+                return False
 
-             while q:
-                for _ in range(len(q)):
-                    curr_r, curr_c=q.popleft()
+            grid[r][c]="0" 
+            
+            for dr, dc in direction:
+                adjr, adjc=(dr+r), (dc+c)
 
-                    for dr, dc in direction:
-                        adj_r, adj_c=(dr+curr_r), (dc+curr_c)
+                dfs(adjr, adjc)
 
-                        if (adj_r, adj_c) not in visit and adj_r in range(totalrows) and adj_c in range(totalcols) and grid[adj_r][adj_c]=="1":
-                            q.append((adj_r, adj_c))
-                            visit.add((adj_r, adj_c))
+                    
 
-        #iterate over the entire matrix
-
-        for row in range(totalrows):
-            for col in range(totalcols):
-                if grid[row][col]=="1" and (row, col) not in visit:
-                    bfs(self, row,col)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    dfs(r,c)
                     island+=1
 
         return island
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # totalrows=len(grid)
+        # totalcols=len(grid[0])
+        # island=0
+
+        # q=deque()
+        # visit=set() #to avoid duplicates, and handle our visited positions
+        # direction=[[0,1], [0,-1], [1,0], [-1,0]]
+
+        # def bfs(self, r, c): #so we will see the adjacent values of the current positon 
+        #      q.append((r, c)) #q=[(0,0)]
+        #      visit.add((r,c)) #we add psoition which is already seen
+             
+
+        #      while q:
+        #         for _ in range(len(q)):
+        #             curr_r, curr_c=q.popleft()
+
+        #             for dr, dc in direction:
+        #                 adj_r, adj_c=(dr+curr_r), (dc+curr_c)
+
+        #                 if (adj_r, adj_c) not in visit and adj_r in range(totalrows) and adj_c in range(totalcols) and grid[adj_r][adj_c]=="1":
+        #                     q.append((adj_r, adj_c))
+        #                     visit.add((adj_r, adj_c))
+
+        # #iterate over the entire matrix
+
+        # for row in range(totalrows):
+        #     for col in range(totalcols):
+        #         if grid[row][col]=="1" and (row, col) not in visit:
+        #             bfs(self, row,col)
+        #             island+=1
+
+        # return island
 
         
         
