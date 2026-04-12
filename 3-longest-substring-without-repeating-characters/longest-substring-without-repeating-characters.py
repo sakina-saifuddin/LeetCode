8ab290d7df1140fdb419efb229aab7ce97a820a5
ML Engineer | Data Analytics & Predictive Modeling <sakinasaifee006@gmail.com>
2026-04-11 20:23:52 -0400

    Time: 8 ms (87.90%) | Memory: 19.3 MB (43.35%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,29 +1,89 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l=0
-        w=0
-        empset=set()
-        r=0
-        longest=0
+
+        maxlen=0
+        l,r=0,0
+        sett=set()
 
         for i, char in enumerate(s):
-            while char in empset:
-                empset.remove(s[l])
-                l=l+1
-
-            empset.add(char)
-            w=(r-l)+1
-            r=r+1
-            longest=max(longest, w)
-        return longest
+            while char in sett:
+                leftchar=s[l]
+                sett.remove(leftchar)
+                l+=1
 
 
 
+            if char not in sett:
+                sett.add(char)
+                wind=(r-l)+1
+                maxlen=max(maxlen, wind)
+                r+=1
+
+        return maxlen
 
 
-sol=Solution()
 
-longestlen=sol.lengthOfLongestSubstring("hello")
 
-print("longestlen", longestlen)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         l=0
+#         w=0
+#         empset=set()
+#         longest=0
+
+#         for i, char in enumerate(s):
+#             while char in empset:
+#                 empset.remove(s[l])
+#                 l=l+1
+
+#             empset.add(char)
+#             w=(i-l)+1
+#             longest=max(longest, w)
+#         return longest
+
+
+
+
+
+# sol=Solution()
+
+# longestlen=sol.lengthOfLongestSubstring("hello")
+
+# print("longestlen", longestlen)
         
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+
+
+#         l=0
+#         max_len=0
+#         empsett=set()
+
+#         for r, char in enumerate(s):
+
+#             while char in empsett:
+#                 empsett.remove(s[l])
+
+#                 l=l+1
+
+
+#             empsett.add(char)
+#             wind=(r-l)+1
+#             max_len=max(max_len, wind)
+
+#         return max_len
+        
+
